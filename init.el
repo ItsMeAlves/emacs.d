ed5115ddc2614ac38c11a42e4a9b9f6a1594df81
Gabriel Alves <gabriel@kazumi.home>
2017-08-09 23:33:36 -0300

    add defuns

--- init.el
+++ init.el
@@ -1,10 +1,40 @@
+
 (package-initialize)
 
-(setq backup-directory-alist '(("" . "~/.emacs-backup")))
+(setq backup-directory-alist '(("" . "~/.saves")))
 
+;; Defuns
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+  Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (untabify-buffer)
+  (delete-trailing-whitespace)
+  (indent-buffer))
+
+(defun new-shell (name)
+  (interactive "sName: ")
+  (pop-to-buffer (concat "*" name "*"))
+  (unless (eq major-mode 'shell-mode)
+    (shell (current-buffer))
+    (sleep-for 0 200)
+    (delete-region (point-min) (point-max))))
+
+(global-set-key (kbd "C-c s") 'new-shell)
 (global-set-key (kbd "C-x p") 'fiplr-find-file)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-;") 'er/expand-region)
+(global-set-key (kbd "C-c c") 'comment-region)
+(global-set-key (kbd "C-c u") 'uncomment-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
 
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
@@ -24,11 +54,11 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-(telephone-line-mode 1)
 (setq telephone-line-primary-right-separator 'telephone-line-abs-left
       telephone-line-secondary-right-separator 'telephone-line-abs-hollow-left)
 (setq telephone-line-height 24
       telephone-line-evil-use-short-tag t)
+(telephone-line-mode 1)
 
 (yas-global-mode 1)
 
@@ -51,21 +81,73 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (monokai)))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#ffffff" "#bf616a" "#B4EB89" "#ebcb8b" "#89AAEB" "#C189EB" "#89EBCA" "#232830"))
+ '(compilation-message-face (quote default))
+ '(custom-enabled-themes (quote (emacs-potpourri)))
  '(custom-safe-themes
    (quote
-    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+    ("7366f03b3f1fd1babd80b5270c6977bcbe8e5016aa0b1ecd3a0925ff00065e03" "604648621aebec024d47c352b8e3411e63bdb384367c3dd2e8db39df81b475f5" "3eb2b5607b41ad8a6da75fe04d5f92a46d1b9a95a202e3f5369e2cdefb7aac5c" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+ '(fci-rule-color "#343d46")
  '(fringe-mode (quote (1 . 1)) nil (fringe))
  '(global-linum-mode t)
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100))))
  '(inhibit-startup-screen t)
  '(linum-format " %d ")
+ '(magit-diff-use-overlays nil)
  '(menu-bar-mode nil)
+ '(notmuch-search-line-faces
+   (quote
+    (("unread" :foreground "#aeee00")
+     ("flagged" :foreground "#0a9dff")
+     ("deleted" :foreground "#ff2c4b" :bold t))))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "http://melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (auto-complete badwolf-theme chess cider counsel elm-mode expand-region fiplr gruvbox-theme haskell-mode ivy js2-mode magit monokai-theme paredit rainbow-delimiters smartparens solarized-theme spacegray-theme sudoku swiper telephone-line vue-html-mode vue-mode web-mode yasnippet zenburn-theme)))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#bf616a")
+     (40 . "#DCA432")
+     (60 . "#ebcb8b")
+     (80 . "#B4EB89")
+     (100 . "#89EBCA")
+     (120 . "#89AAEB")
+     (140 . "#C189EB")
+     (160 . "#bf616a")
+     (180 . "#DCA432")
+     (200 . "#ebcb8b")
+     (220 . "#B4EB89")
+     (240 . "#89EBCA")
+     (260 . "#89AAEB")
+     (280 . "#C189EB")
+     (300 . "#bf616a")
+     (320 . "#DCA432")
+     (340 . "#ebcb8b")
+     (360 . "#B4EB89"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
